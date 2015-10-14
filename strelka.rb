3697a8463011c139c22b3d14390482eb33481a92
Bruno Grande <bgrande@sfu.ca>
2015-10-14 07:34:25 -0700

    added fails_with for clang

--- strelka.rb
+++ strelka.rb
@@ -4,6 +4,7 @@ class Strelka < Formula
   url "ftp://strelka:%27%27@ftp.illumina.com/v1-branch/v1.0.14/strelka_workflow-1.0.14.tar.gz"
   sha256 "b3aeca63dfea7787a4046a0ff747e9fe4a267297a3d0ae5126fab890e7152f39"
 
+  # 
   # Needs Python for testing installation
   depends_on :python => :build
   # Needs rsync for installing to #{prefix}
@@ -15,6 +16,15 @@ class Strelka < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  fails_with :clang do
+    build 700
+    cause <<-EOS.undent
+      The version of samtools redistributed with Strelka fails to compile with
+      clang on OS X due to missing symbols. See link for more details:
+      https://groups.google.com/d/topic/strelka-discuss/bS3TgY5YfxE/discussion
+      EOS
   end
 
   test do
