e6aaf63b402a25c7d95a47c03c0af10e6e5a6494
Bruno Grande <bgrande@sfu.ca>
2015-10-13 21:50:12 -0700

    added python dep and shortened desc

--- strelka.rb
+++ strelka.rb
@@ -1,8 +1,11 @@
 class Strelka < Formula
-  desc "Somatic variant calling workflow for matched tumor-normal samples"
+  desc "Somatic variant calling workflow for tumor samples"
   homepage "https://sites.google.com/site/strelkasomaticvariantcaller/home"
   url "ftp://strelka:%27%27@ftp.illumina.com/v1-branch/v1.0.14/strelka_workflow-1.0.14.tar.gz"
   sha256 "b3aeca63dfea7787a4046a0ff747e9fe4a267297a3d0ae5126fab890e7152f39"
+
+  # Needs Python for testing
+  depends_on :python => :build
 
   def install
     system "./configure", "--disable-debug",
